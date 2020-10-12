bd6f3dd841f9020973e1e9e2202dc386426ba74f
Gabriel <gabrielhondacba@gmail.com>
2020-10-12 19:21:22 -0400

    wip: criado banco de dados em memória para categoria

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+
+export class InMemoryDatabase implements InMemoryDbService{
+  createDb(reqInfo?: RequestInfo): {} | Observable<{}> | Promise<{}>{
+    const categories = [
+      {id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa'},
+      {id: 1, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
+      {id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc'},
+      {id: 1, name: 'Salário', description: 'Recebimento de Salário'},
+      {id: 1, name: 'Freelas', description: 'Trabalhos como freelancer'},
+    ];
+    
+    return {categories};
+  }
+  
+  
+}
