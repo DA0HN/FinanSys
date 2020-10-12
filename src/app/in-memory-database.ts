4f321d43c478415e064d05bd0003c569221a7441
Gabriel <gabrielhondacba@gmail.com>
2020-10-12 19:24:19 -0400

    wip: criado modelo de categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,10 +1,11 @@
 import { Observable } from 'rxjs';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared';
 
 
 export class InMemoryDatabase implements InMemoryDbService{
   createDb(reqInfo?: RequestInfo): {} | Observable<{}> | Promise<{}>{
-    const categories = [
+    const categories: Category[] = [
       {id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa'},
       {id: 1, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
       {id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc'},
