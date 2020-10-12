097f70669b6ae51ae74cc1fe6b55afa56e01116a
Gabriel <gabrielhondacba@gmail.com>
2020-10-12 10:41:34 -0400

    feat: implementado rotas para categoria

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CategoriesRoutingModule } from './categories-routing.module';
-import { CategoryListComponent } from './category-list/category-list.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
+import {CategoriesRoutingModule} from './categories-routing.module';
+import {CategoryListComponent} from './category-list';
+import {CategoryFormComponent} from './category-form';
 
 
 @NgModule({
-  declarations: [CategoryListComponent, CategoryFormComponent],
+  declarations: [
+    CategoryListComponent,
+    CategoryFormComponent
+  ],
   imports: [
     CommonModule,
     CategoriesRoutingModule
   ]
 })
-export class CategoriesModule { }
+export class CategoriesModule {
+}
