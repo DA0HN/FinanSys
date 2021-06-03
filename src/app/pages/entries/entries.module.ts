980018e3123d0820be9c473ebf50541ff077f4b1
Gabriel Honda <gabrielhondacba@gmail.com>
2021-06-03 16:08:30 -0400

    feat: declara componente EntryForm

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -4,11 +4,13 @@ import {CommonModule} from '@angular/common';
 import {EntriesRoutingModule} from './entries-routing.module';
 import {EntryService} from './shared';
 import {EntryListComponent} from './entry-list';
+import {EntryFormComponent} from '@/app/pages/entries/entry-form';
 
 
 @NgModule({
   declarations: [
-    EntryListComponent
+    EntryListComponent,
+    EntryFormComponent,
   ],
   imports: [
     CommonModule,
