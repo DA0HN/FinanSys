3db3fc0e00b118a450407190dca63fcbc8847cb7
Gabriel Honda <gabrielhondacba@gmail.com>
2021-06-03 16:08:46 -0400

    feat: adiciona rotas de criação e edição para EntryForm

--- src/app/pages/entries/entries-routing.module.ts
+++ src/app/pages/entries/entries-routing.module.ts
@@ -1,11 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EntryListComponent} from '@/app/pages/entries/entry-list';
+import {EntryFormComponent} from '@/app/pages/entries/entry-form';
 
 const routes: Routes = [
   {
     path: '',
     component: EntryListComponent
+  },
+  {
+    path: 'new',
+    component: EntryFormComponent
+  },
+  {
+    path: ':id/edit',
+    component: EntryFormComponent
   },
 ];
 
