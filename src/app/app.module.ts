b70d309482733635f6be1f20b7055b1f85878ff5
Gabriel Honda <gabriel.honda@totemti.com.br>
2022-10-12 20:40:11 -0400

    refactor: import one time core modules in `AppModule`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from '@finan$ys/app.component';
 import { AppRoutingModule } from '@finan$ys/app-routing.module';
-import { InMemoryDatabase } from '@finan$ys/in-memory-database';
+import { CoreModule } from '@finan$ys/core/core.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    CoreModule,
   ],
   providers: [],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [ AppComponent ],
 })
 export class AppModule {
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDatabase } from '@finan$ys/in-memory-database';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+  ],
+  exports: [
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
 })
-export class CoreModule { }
+export class CoreModule {
+}
