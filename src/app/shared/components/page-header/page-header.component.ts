0420ebb460cc605ce71de439505a8678bc7b637c
Da0hn <gabrielhondacba@gmail.com>
2022-10-16 10:29:40 -0400

    refactor: add dynamic button style to button header

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-header[title][btnLabel][btnLink]',
+  selector: 'app-page-header[title][btnLabel][btnLink][btnClass]',
   templateUrl: './page-header.component.html',
   styleUrls: [ './page-header.component.css' ],
 })
@@ -10,6 +10,7 @@ export class PageHeaderComponent implements OnInit {
   @Input() title: string;
   @Input() btnLabel: string;
   @Input() btnLink: string;
+  @Input() btnClass: string;
 
   constructor() {
   }
